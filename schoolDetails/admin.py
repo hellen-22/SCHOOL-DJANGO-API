84f8667257b921f64bcc51e2045ff0e780db976c
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-10-11 10:37:51 +0300

    creating the remaining models

--- schoolDetails/admin.py
+++ schoolDetails/admin.py
@@ -9,8 +9,35 @@ class SchoolAdmin(admin.ModelAdmin):
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['school_name', 'department_name']
+    autocomplete_fields = ['school']
 
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(UnitDetails)
+class UnitDetailsAdmin(admin.ModelAdmin):
+    list_display = ['unit_name', 'registration_status']
+    autocomplete_fields = ['unit']
+
+@admin.register(Hostel)
+class HostelAdmin(admin.ModelAdmin):
+    list_display = ['hostel_name', 'capacity']
+
+@admin.register(StudentHostel)
+class StudentHostelAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'payment_status']
+    autocomplete_fields = ['student', 'hostel', 'payment_status']
+
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['date', 'unit_name', 'reg_no']
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
     pass
--- schoolDetails/models.py
+++ schoolDetails/models.py
@@ -1,19 +1,20 @@
 from random import choices
-from turtle import mode
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+import validators
 
 from account.models import *
 
 
 class School(models.Model):
-    name = models.CharField(max_length=255)
+    school_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.name
 
 class Department(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='school')
-    name = models.CharField(max_length=255)
+    department_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.name
@@ -26,10 +27,76 @@ class Unit(models.Model):
         return self.unit_name
 
 class UnitDetails(models.Model):
-    REGISTRATION = (
+    REGISTRATION_CHOICES = (
         ('Y', 'Yes'),
         ('N', 'No')
     )
     unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='units')
-    student_id = models.ForeignKey('account.Student', on_delete=models.CASCADE)
-    registration_status = models.CharField(max_length=255, choices=REGISTRATION)
+    student_id = models.ForeignKey('account.Student', on_delete=models.CASCADE, related_name='units')
+    registration_status = models.CharField(max_length=255, choices=REGISTRATION_CHOICES)
+
+    def __str__(self) -> str:
+        return self.unit.unit_name 
+
+    
+class Hostel(models.Model):
+    hostel_name = models.CharField(max_length=200)
+    capacity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
+
+    def __str__(self) -> str:
+        return self.hostel_name
+
+    class Meta:
+        ordering = ['capacity']
+
+
+class StudentHostel(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    student = models.ForeignKey('account.Student', on_delete=models.CASCADE, related_name='student_hostel')
+    hostel = models.ForeignKey(Hostel, on_delete=models.CASCADE, related_name='hostel')
+    payment_status = models.CharField(max_length=200, choices=PAYMENT_STATUS_CHOICES)
+
+    def __str__(self) -> str:
+        return f'{self.student.first_name} {self.student.last_name}'
+
+
+class Exam(models.Model):
+    EXAM_TYPE_CHOICES = (
+        ('SUP', 'SUPPLEMENTARY EXAM'),
+        ('SPE', 'SPECIAL EXAM'),
+        ('RE', 'RETAKE EXAM')
+        ('MAIN', 'MAIN EXAM')
+    )
+    exam_type = models.CharField(max_length=200, choices=EXAM_TYPE_CHOICES)
+    date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return self.exam_type
+
+class Result(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    student = models.ForeignKey('account.Student', on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    cat = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(30)])
+    exam = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(70)])
+
+    def __str__(self) -> str:
+        return self.exam.exam_type
+
+class Attendance(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey('account.Student', on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.date
+
+    
