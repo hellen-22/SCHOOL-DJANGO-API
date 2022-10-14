7a4f5ffeef4bcaf775b4a9de38b361845148aa7c
Hellen Wainaina <58620060+hellen-22@users.noreply.github.com>
2022-10-14 11:17:58 +0300

    saving user

--- account/serializers.py
+++ account/serializers.py
@@ -1,37 +1,64 @@
-import email
+from django.db import transaction
 from rest_framework import serializers
 
 from .models import *
 from schoolDetails.serializers import UnitSerializer
 from custom.models import User
+from custom.serializers import UserCreateSerializer
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'first_name']
 
 
 class StudentSerializer(serializers.ModelSerializer):
-    user = UserDetailsSerializer()
-    def create(self, validated_data):
-        user = self.validated_data['user']
-        
-        if not Lecturer.objects.filter(user=user).count() == 0:
-            raise serializers.ValidationError("User is already a lecturer")
-        
-        return Student.objects.create(**validated_data)
+    user = UserCreateSerializer()
+
+    def save(self, **kwargs):
+        with transaction.atomic:
+            user = dict(self.validated_data['user'])
+            username = user['username']
+            email = user['email']
+            first_name = user['first_name']
+            last_name = user['last_name']
+            password = user['password']
+
+            student_reg_no = self.validated_data['reg_no']
+            student_department = self.validated_data['department']
+
+            user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
+            
+            return Student.objects.create(reg_no=student_reg_no, department=student_department, user=user)
 
     class Meta:
         model = Student
         fields = ['id', 'reg_no', 'department', 'user']
 
+
+
 class LecturerSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        user = validated_data['user']
-        if not Student.objects.filter(user=user).count() == 0:
-            raise serializers.ValidationError("User is already a lecturer")
-        
-        return Lecturer.objects.create(**validated_data)
+    user = UserCreateSerializer()
+
+    def validate_user(self, value):
+        if User.objects.filter(pk=value).exists():
+            raise serializers.ValidationError('User already exists')
+
+    def save(self, **kwargs):
+        with transaction.atomic:
+            user = dict(self.validated_data['user'])
+            username = user['username']
+            email = user['email']
+            first_name = user['first_name']
+            last_name = user['last_name']
+            password = user['password']
+
+            lecturer_staff_id = self.validated_data['staff_id']
+            lecturer_department = self.validated_data['department']
+
+            user = User.objects.create_user(username=username, email=email, first_name=first_name, last_name=last_name, password=password)
+            
+            return Lecturer.objects.create(staff_id=lecturer_staff_id, department=lecturer_department, user=user)
 
     class Meta:
         model = Lecturer
